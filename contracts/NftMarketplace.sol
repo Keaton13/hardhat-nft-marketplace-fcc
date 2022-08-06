a7e446eb825c355e8bc06a2218d8100a34d22a8b
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2022-08-06 11:01:40 -0700

    Building NftMarketplace contract

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -3,10 +3,55 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-error NftMarketplace_PriceMustBeAboveZero();
-error NftMarketplace_NotApprovedForMarketplace();
+error NftMarketplace__PriceMustBeAboveZero();
+error NftMarketplace__NotApprovedForMarketplace();
+error NftMarketplace__AlreadyListed(address nftAddress, uint256 tokenId);
+error NftMarketplace__NotOwner();
 
 contract NftMarketplace {
+    struct Listing {
+        uint256 price;
+        address seller;
+    }
+
+    event ItemListed(
+        address indexed seller,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+
+    // NFT Contract address -> NFT TokenID -> Listing
+    mapping(address => mapping(uint256 => Listing)) private s_listings;
+
+    ////////////////////
+    // Moddifiers //
+    ///////////////////
+    modifier notListed(
+        address nftAddress,
+        uint256 tokenId,
+        address owner
+    ) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if (listing.price > 0) {
+            revert NftMarketplace__AlreadyListed(nftAddress, tokenId);
+        }
+        _;
+    }
+
+    modifier isOwner(
+        address nftAddress,
+        uint256 tokenId,
+        address spender
+    ) {
+        IERC721 nft = IERC721(nftAddress);
+        address owner = nft.ownerOf(tokenId);
+        if (spender != owner) {
+            revert NftMarketplace__NotOwner();
+        }
+        _;
+    }
+
     ////////////////////
     // Main Functions //
     ///////////////////
@@ -14,18 +59,19 @@ contract NftMarketplace {
         address nftAddress,
         uint256 tokenId,
         uint256 price
-    ) external {
-        if (price <= 0){
-            revert NftMarketplace_PriceMustBeAboveZero();
+    ) external notListed(nftAddress, tokenId, msg.sender) isOwner(nftAddress, tokenId, msg.sender) {
+        if (price <= 0) {
+            revert NftMarketplace__PriceMustBeAboveZero();
         }
         // 1. Send the NFT to the contract. Transfer -> Contract "hold" the NFT.
         // 2. Owners can still hold their NFT, and give the marketplace approval
         // to sell the NFT for them.
         IERC721 nft = IERC721(nftAddress);
-        if (nft.getApproved(tokenId) != address(this)){
-            revert NftMarketplace_NotApprovedForMarketplace();
+        if (nft.getApproved(tokenId) != address(this)) {
+            revert NftMarketplace__NotApprovedForMarketplace();
         }
-        //
+        s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
+        emit ItemListed(msg.sender, nftAddress, tokenId, price);
     }
 }
 
